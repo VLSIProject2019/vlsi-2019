1061817e5be46f4aa01db3d3050cfbde4cae8c35
cnorfleet0 <cnorfleet0@gmail.com>
2019-01-16 22:08:02 -0500

    added simple alu

--- processor.sv
+++ processor.sv
@@ -1,6 +1,14 @@
 // microprocessor
 
-module top  ();
+module top ();
+endmodule
+
+module alu (input logic [3:0]  d0, d1,
+				input logic        subtractionControl,
+				output logic [3:0] result);
+	logic [3:0] condinvb;
+	assign condinvb = subtractionControl ? ~b : b;
+	assign result = a + condinvb + subtractionControl;
 endmodule
 
 module mux2 #(parameter WIDTH = 4)
