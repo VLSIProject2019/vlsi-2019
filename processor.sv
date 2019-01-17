9faeb4b1d01bece89d389005c5e5efaa937cef4d
cnorfleet0 <cnorfleet0@gmail.com>
2019-01-16 22:02:27 -0500

    added mux2

--- processor.sv
+++ processor.sv
@@ -1,1 +1,11 @@
 // microprocessor
+
+module top  ();
+endmodule
+
+module mux2 #(parameter WIDTH = 4)
+				(input  logic [WIDTH-1:0] d0, d1, 
+				 input  logic             s, 
+				 output logic [WIDTH-1:0] y);
+	assign y = s ? d1 : d0; 
+endmodule
