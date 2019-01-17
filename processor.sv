b3712f41cf14fd45e3c38605f373172514046120
cnorfleet0 <cnorfleet0@gmail.com>
2019-01-17 10:51:43 -0500

    added regfile with 4 registers

--- processor.sv
+++ processor.sv
@@ -20,6 +20,22 @@ module alu (input logic [3:0]  d0, d1,
 	assign result = a + condinvb + subtractionControl;
 endmodule
 
+module regfile(input  logic       clk, 
+               input  logic       we3, 
+               input  logic [1:0] ra1, ra2, wa3, 
+               input  logic [3:0] wd3,
+               output logic [3:0] rd1, rd2);
+	// note: can't read PC in HMMM
+	
+	logic [3:0] rf[1:0];
+	
+	always_ff @(posedge clk)
+		if (we3) rf[wa3] <= wd3;
+	
+	assign rd1 = rf[ra1];
+	assign rd2 = rf[ra2];
+endmodule
+
 module flopr #(parameter WIDTH = 8)
               (input  logic             clk, reset,
                input  logic [WIDTH-1:0] d, 
