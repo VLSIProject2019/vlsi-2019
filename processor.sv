b60a4c13fea98dc54c4abf6304612428ca4ffadf
cnorfleet0 <cnorfleet0@gmail.com>
2019-01-17 10:47:19 -0500

    added resetable register

--- processor.sv
+++ processor.sv
@@ -20,6 +20,15 @@ module alu (input logic [3:0]  d0, d1,
 	assign result = a + condinvb + subtractionControl;
 endmodule
 
+module flopr #(parameter WIDTH = 8)
+              (input  logic             clk, reset,
+               input  logic [WIDTH-1:0] d, 
+               output logic [WIDTH-1:0] q);
+	always_ff @(posedge clk, posedge reset)
+		if (reset) q <= 0;
+		else       q <= d;
+endmodule
+
 module mux2 #(parameter WIDTH = 4)
 				(input  logic [WIDTH-1:0] d0, d1, 
 				 input  logic             s, 
