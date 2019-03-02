36f1b8c6bd6eb7ac66662edc2d7d48eda56a2ff7
cnorfleet0 <cnorfleet0@gmail.com>
2019-03-02 00:16:41 -0800

    completed datapath v1

--- processor.sv
+++ processor.sv
@@ -3,52 +3,63 @@
 module top (input logic        clk, reset,
 				output logic       memWrite,
 				output logic [7:0] adr,
-				input logic [9:0]  instruct,
-				input logic [3:0]  ReadData);
-	logic[3:0] branchRegVal;
-	logic PCS, RegWrite, MemWrite, ALUOp, ALUSrc;
-	logic[1:0] RegWriteSrc;
+				input logic  [9:0] instruct,
+				input logic  [3:0] ReadData);
+	//logic[3:0] branchRegVal;
+	//logic PCS, RegWrite, MemWrite, ALUOp, ALUSrc;
+	//logic[1:0] RegWriteSrc;
 	
-	controller c(clk, reset, instruct[9:6],
-					 branchRegVal,
-					 PCS, RegWrite, MemWrite,
-					 ALUOp, ALUSrc, RegWriteSrc);
-	datapath dp(clk, reset, instruct[5:0],
-				  PCS, RegWrite, MemWrite,
-				  ALUOp, ALUSrc, RegWriteSrc,
-				  ReadData, branchRegVal);
+	logic MemWrite;
+	
+	//memory(MemWrite, Adr, WriteData, ReadData);
+	
+	//controller c(clk, reset, instruct[9:6],
+	//				 branchRegVal,
+	//				 PCS, RegWrite, MemWrite,
+	//				 ALUOp, ALUSrc, RegWriteSrc);
+	datapath dp(clk, reset, PCEnable, AdrSrc, InstrSrc,
+					RegWrite, TwoRegs, ALUSub, PCSrc,
+					RegWriteSrc, ReadData, Adr, WriteData);
 endmodule
 
-module datapath (input logic       clk, reset,
-					  input logic[5:0]  instruct,
-					  input logic       PCS, RegWrite, MemWrite,
-					  input logic       ALUOp, ALUSrc,
-					  input logic[1:0]  RegWriteSrc,
-					  input logic[3:0]  ReadData,
-					  output logic[3:0] branchRegVal);
-	logic[7:0] PC, PCNext, PCPlus1;
-	logic[3:0] Result, SrcA, SrcB;
-	logic[3:0] RA1, RA2, WA3, WD3, RD1, RD2;
-	logic[3:0] extImm;
+module datapath (input  logic        clk, reset,
+					  input  logic        PCEnable, AdrSrc, InstrSrc,
+					  input  logic        RegWrite, TwoRegs, ALUSub,
+					  input  logic [1:0]  PCSrc, RegWriteSrc,
+					  input  logic [14:0] ReadData,
+					  output logic [7:0]  Adr, WriteData);
+	logic[7:0]  PC, PCNext, PCPlus1;
+	logic[7:0]  Result, SrcA, SrcB, Imm;
+	logic[7:0]  WD3, RD1, RD2, notRD2;
+	logic[3:0]  RA1, RA2, WA3;
+	logic[15:0] instrTemp, instr;
 	
 	// next PC logic
-	adder #(8) pcAdd(PC, 8'b1, PCPlus1);
-	flopr #(8) pcReg(clk, reset, PCNext, PC);
+	adder   #(8) pcAdd(PC, 8'b1, 1'b0, PCPlus1);
+	flopenr #(8) pcReg(clk, reset, PCNext, PC);
+	mux3    #(8) pcMux(PCPlus1, Imm, RD1, PCSrc, PCNext);
+	
+	// data memory
+	mux2 #(8) adrMux(PC, RD2, AdrSrc, Adr);
+	assign WriteData = RD1;
+	
+	//instruction handling
+	flopr #(15) instrReg(clk, reset, ReadData, instrTemp);
+	mux2  #(15) instrMux(instrTemp, ReadData, InstrSrc, instr);
 	
 	//register read/write logic
-	assign WA3 = instruct[5:4];
-	assign RA1 = instruct[3:2];
-	assign RA2 = instruct[1:0];
-	mux3 #(4) wd3Mux(Result, ReadData, extImm, RegWriteSrc, WD3);
-	regfile rf(clk, RegWrite, RA1, RA2,
-				  WA3, WD3, RD1, RD2);
-	assign branchRegVal = RD1;
+	mux3 #(8)  wd3Mux(imm, ReadData[7:0], Result, RegWriteSrc, WD3);
+	regfile rf(clk, RegWrite, RA1, RA2, WA3, WD3, RD1, RD2);
+	assign RA1 = instr[9:7];
+	assign RA2 = instr[12:10];
+	assign WA3 = instr[6:4];
+	assign imm = instr[7:14];
 	
 	// ALU logic
-	mux2 #(4) srcAMux(4'b0, RD1, ALUSrc, SrcA);
-	mux2 #(4) srcBMux(RD1, RD2, ALUSrc, SrcB);
-	alu alu(SrcA, SrcB, ALUOp, Result);
-	
+	mux2 #(8) srcAMux(8'b0, RD1, TwoRegs, SrcA);
+	assign notRD2 = ~RD2;
+	mux2 #(8) srcBMux(RD2, notRD2, ALUSub, SrcB);
+	adder #(8) alu(SrcA, SrcB, ALUSub, Result);
 endmodule
 
 module controller (input logic       clk, reset,
@@ -101,32 +112,43 @@ module condcheck (input logic[1:0] branchType,
 		endcase
 endmodule
 
-module alu (input logic [3:0]  a, b,
+/*module alu (input logic [3:0]  a, b,
 				input logic        subtractionControl,
 				output logic [3:0] result);
 	logic [3:0] condinvb;
 	assign condinvb = subtractionControl ? ~b : b;
 	assign result = a + condinvb + subtractionControl;
-endmodule
+endmodule*/
 
 module adder #(parameter WIDTH=8)
               (input  logic [WIDTH-1:0] a, b,
+				   input  logic cin,
                output logic [WIDTH-1:0] y);
-	assign y = a + b;
+	assign y = a + b + cin;
 endmodule
 
 module regfile(input  logic       clk, 
                input  logic       we3, 
-               input  logic [1:0] ra1, ra2, wa3, 
-               input  logic [3:0] wd3,
-               output logic [3:0] rd1, rd2);
+               input  logic [3:0] ra1, ra2, wa3, 
+               input  logic [7:0] wd3,
+               output logic [7:0] rd1, rd2);
 	// note: can't read PC in HMMM
-	logic [3:0] rf[1:0];
+	logic [7:0] rf[7:0];
 	
 	always_ff @(posedge clk)
 		if (we3) rf[wa3] <= wd3;
 	assign rd1 = rf[ra1];
 	assign rd2 = rf[ra2];
+endmodule
+
+module flopenr #(parameter WIDTH = 8)
+                (input  logic             clk, reset, en,
+                 input  logic [WIDTH-1:0] d, 
+                 output logic [WIDTH-1:0] q);
+
+  always_ff @(posedge clk, posedge reset)
+    if (reset)   q <= 0;
+    else if (en) q <= d;
 endmodule
 
 module flopr #(parameter WIDTH = 8)
