d9c0485e3d1553e3280913e0b43f01e0e15a4410
cnorfleet0 <cnorfleet0@gmail.com>
2019-03-11 11:51:04 -0700

    comment cleanup

--- processor.sv
+++ processor.sv
@@ -1,18 +1,11 @@
 // Microprocessor for E190AT: VLSI Design Project
-// Authors: Erik Meike, Dominique Mena, Caleb Norfleet, & Kaveh Pezeshki
+// Authors: Erik Meike, Caleb Norfleet, & Kaveh Pezeshki
 // Created Spring 2019
 
-// note: switch to two-phase clock
 module top (input  logic        ph1, ph2, reset,
 				output logic        MemWrite,
 				output logic [7:0]  Adr,
 				inout  logic [14:0] MemData);
-				// input  logic [14:8] MemData1,
-				// inout  logic [7:0]  MemData2);
-	// logic [14:0] MemData;
-	// assign MemData[14:8] = MemData1;
-	// assign MemData[7:0]  = MemData2;
-	// memory(MemWrite, Adr, MemData);
 	
 	logic PCEnable, AdrSrc, InstrSrc, RegWrite, TwoRegs, ALUSub;
 	logic [1:0] PCSrc, RegWriteSrc;
