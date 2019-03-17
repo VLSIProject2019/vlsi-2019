35f0a0c8eaabd6c69353f96c426a3fa370741f61
cnorfleet0 <cnorfleet0@gmail.com>
2019-03-17 18:01:47 -0400

    fixed PC reset on start

--- processor.sv
+++ processor.sv
@@ -189,7 +189,10 @@ module mux3 #(parameter WIDTH = 8)
 				 (input  logic [WIDTH-1:0] d0, d1, d2,
               input  logic [1:0]       s, 
               output logic [WIDTH-1:0] y);
-	assign y = s[1] ? (s[0] ? 8'bx : d2) : (s[0] ? d1 : d0);
+	// note: 11 is treated as the same as 10
+	assign y = s[1] ? (d2) : (s[0] ? d1 : d0);
+	// alternative:
+	// assign y = s[1] ? (s[0] ? 8'bx : d2) : (s[0] ? d1 : d0);
 endmodule
 
 module flop #(parameter WIDTH = 8)
